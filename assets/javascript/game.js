b35388455545c0e554027012748bcfd9783e80a3
Kenneth Chung <kennethchung5@gmail.com>
2018-10-12 22:21:57 -0400

    introduced livePlay variable to improve onkeyup handling; added Guess# images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,6 +50,7 @@ var currentWord = [];
 var guessedLetters = [];
 var remainingGuesses;
 var hitCount;
+var livePlay = 0;
 
 function setWord() {
     // check to see if words remain (i.e. wordSet not empty)
@@ -78,49 +79,58 @@ function setWord() {
     guessedLetters = [];
     remainingGuesses = 10; 
     hitCount = 0;
+    livePlay = 1;
 };
 
 
 //respond to keyup
 document.onkeyup = function checkLetter(event) {
-    var currentLetter = event.key.toUpperCase();        
+    // execute only if game is ready (livePlay === 1). this prevents responding to keyups when game is in a certain state.
+    if (livePlay === 1) {
+        var currentLetter = event.key.toUpperCase();        
 
-    // check that: 1.key is a letter and 2.key is not already guessed
-    if (allLetters.indexOf(currentLetter) > -1 && guessedLetters.indexOf(currentLetter) === -1) {
-        var correctLetter = 0;
+        // check that: 1.key is a letter and 2.key is not already guessed
+        if (allLetters.indexOf(currentLetter) > -1 && guessedLetters.indexOf(currentLetter) === -1) {
+            var correctLetter = 0;
 
-        // loop through currentWord, check match against currentLetter
-        for (i = 0; i < currentWord.length; i++) {
-            if (currentWord[i] === currentLetter) {
-                // reveal letter in the corresponding letterDiv
-                document.getElementById("letterDiv" + i).textContent = currentLetter;                
+            // loop through currentWord, check match against currentLetter
+            for (i = 0; i < currentWord.length; i++) {
+                if (currentWord[i] === currentLetter) {
+                    // reveal letter in the corresponding letterDiv
+                    document.getElementById("letterDiv" + i).textContent = currentLetter;                
+                    
+                    hitCount += 1;
+                    correctLetter = 1;
+                };
+            }
                 
-                hitCount += 1;
-                correctLetter = 1;
-            };
-        }
+            // add currentLetter to guessedLetters array
+            guessedLetters.push(currentLetter);
+            // additionally: display in guessedList
+            document.getElementById("guessedList").textContent = guessedLetters;
             
-        // add currentLetter to guessedLetters array
-        guessedLetters.push(currentLetter);
-        // additionally: display in guessedList
-        document.getElementById("guessedList").textContent = guessedLetters;
-         
-        //based on whether a correct guess was made, different functions/methods to execute. 
-        if (correctLetter === 1) {
-            //style this element in guessedLetters
+            //based on whether a correct guess was made, different functions/methods to execute. 
+            if (correctLetter === 1) {
+                //style this element in guessedLetters
 
-            //check whether more letters remain; if not, increase the score
-            if (hitCount === currentWord.length) {
-                score += 1;
-                setWord();
-            };
-        }
-        else {
-            remainingGuesses -= 1;
+                //check whether more letters remain; if not, increase the score
+                if (hitCount === currentWord.length) {
+                    score += 1;
 
-            if(remainingGuesses === 0) {
-                setWord();
-            };
-        };        
+                    livePlay = 0;
+                    // add event for setword? maybe: call a function into solved state
+                    // setWord();
+                };
+            }
+            else {
+                remainingGuesses -= 1;
+
+                if(remainingGuesses === 0) {
+                    livePlay = 0;
+                    // add event for setword? maybe: call a function into solved state
+                    // setWord();
+                };
+            };        
+        };
     };
 };
