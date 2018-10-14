17476a086a355f555e49db06815ab721d181f264
Kenneth Chung <kennethchung5@gmail.com>
2018-10-13 23:15:29 -0400

    handled end of game (no more words)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -46,32 +46,32 @@ var wordSet = [
     ["C","O","M","M","U","T","E"],
     ["C","O","N","D","U","C","T","O","R"],
     ["D","E","L","A","Y","E","D"],
-    ["D","R","I","V","E","R"],
-    ["E","X","P","R","E","S","S"],
-    ["F","A","R","E"],
-    ["L","I","M","I","T","E","D"],
-    ["L","I","N","E"],
-    ["L","O","C","A","L"],
-    ["M","E","T","R","O","C","A","R","D"],
-    ["P","A","S","S","E","N","G","E","R"],
-    ["P","L","A","T","F","O","R","M"],
-    ["R","A","I","L"],
-    ["S","E","R","V","I","C","E"],
-    ["S","I","G","N","A","L"],
-    ["S","T","A","T","I","O","N"],
-    ["S","T","O","P"],
-    ["S","T","R","A","P","H","A","N","G","E","R"],
-    ["S","U","B","W","A","Y"],
-    ["S","W","I","P","E"],
-    ["T","E","R","M","I","N","A","L"],
-    ["T","R","A","C","K"],
-    ["T","R","A","I","N"],
-    ["T","R","A","N","S","F","E","R"],
-    ["T","R","A","N","S","I","T"],
-    ["T","U","N","N","E","L"],
-    ["T","U","R","N","S","T","I","L","E"],
-    ["U","N","D","E","R","G","R","O","U","N","D"],
-    ["U","N","L","I","M","I","T","E","D"]
+    ["D","R","I","V","E","R"]
+    // ["E","X","P","R","E","S","S"],
+    // ["F","A","R","E"],
+    // ["L","I","M","I","T","E","D"],
+    // ["L","I","N","E"],
+    // ["L","O","C","A","L"],
+    // ["M","E","T","R","O","C","A","R","D"],
+    // ["P","A","S","S","E","N","G","E","R"],
+    // ["P","L","A","T","F","O","R","M"],
+    // ["R","A","I","L"],
+    // ["S","E","R","V","I","C","E"],
+    // ["S","I","G","N","A","L"],
+    // ["S","T","A","T","I","O","N"],
+    // ["S","T","O","P"],
+    // ["S","T","R","A","P","H","A","N","G","E","R"],
+    // ["S","U","B","W","A","Y"],
+    // ["S","W","I","P","E"],
+    // ["T","E","R","M","I","N","A","L"],
+    // ["T","R","A","C","K"],
+    // ["T","R","A","I","N"],
+    // ["T","R","A","N","S","F","E","R"],
+    // ["T","R","A","N","S","I","T"],
+    // ["T","U","N","N","E","L"],
+    // ["T","U","R","N","S","T","I","L","E"],
+    // ["U","N","D","E","R","G","R","O","U","N","D"],
+    // ["U","N","L","I","M","I","T","E","D"]
 
 
 ];
@@ -84,43 +84,56 @@ var livePlay = false;
 
 
 
-function setWord() {
-    
-        // check to see if words remain (i.e. wordSet not empty)
+function setWord() {    
 
         // clear page (wordDisplay and guessedList)
         document.getElementById("wordDisplay").innerHTML = "";
         document.getElementById("guessedList").innerHTML = "";
         document.getElementById("resultText").innerHTML = "";
 
-        // pick word from wordSet
-        var randomIndex = Math.floor(Math.random() * wordSet.length);
-        currentWord = wordSet[randomIndex];
+        if (wordSet.length === 0) {
+            livePlay = false;
+
+            document.getElementById("GuessNumberImg").setAttribute("src", "");
+            document.getElementById("GuessNumberImg").setAttribute("alt", "");
+
+            document.getElementById("resultImg").setAttribute("src", "assets/images/EndGame.PNG");
+            document.getElementById("resultImg").setAttribute("alt", "Thanks for playing!");
+
+            document.getElementById("pTop").textContent = "No more words!";
+            document.getElementById("pBottom").textContent = "";
+            document.getElementById("resultText").textContent = "Thanks for playing!";
+        }
+        else {
+            // pick word from wordSet
+            var randomIndex = Math.floor(Math.random() * wordSet.length);
+            currentWord = wordSet[randomIndex];
 
 
-        // remove from array
-        wordSet.splice(randomIndex, 1);
+            // remove from array
+            wordSet.splice(randomIndex, 1);
 
-        // set word in display
-        for (var i = 0; i < currentWord.length; i++) {
-            var letterBox = document.createElement("div");
-            letterBox.setAttribute("class", "letterDiv");
-            letterBox.id = "letterDiv" + i;
-            letterBox.textContent = "__";
-            document.getElementById("wordDisplay").appendChild(letterBox);
+            // set word in display
+            for (var i = 0; i < currentWord.length; i++) {
+                var letterBox = document.createElement("div");
+                letterBox.setAttribute("class", "letterDiv");
+                letterBox.id = "letterDiv" + i;
+                letterBox.textContent = "__";
+                document.getElementById("wordDisplay").appendChild(letterBox);
+            };
+
+            // set Guess# to 7
+            document.getElementById("GuessNumberImg").setAttribute("src", "assets/images/Guess7.PNG");
+            document.getElementById("GuessNumberImg").setAttribute("alt", "7");
+            document.getElementById("resultImg").setAttribute("src", "");
+            document.getElementById("resultImg").setAttribute("alt", "");
+            
+            // initiate per-word variables
+            guessedLetters = [];
+            remainingGuesses = 7; 
+            hitCount = 0;
+            livePlay = true;
         };
-
-        // set Guess# to 7
-        document.getElementById("GuessNumberImg").setAttribute("src", "assets/images/Guess7.PNG");
-        document.getElementById("GuessNumberImg").setAttribute("alt", "7");
-        document.getElementById("resultImg").setAttribute("src", "");
-        document.getElementById("resultImg").setAttribute("alt", "");
-        
-        // initiate per-word variables
-        guessedLetters = [];
-        remainingGuesses = 7; 
-        hitCount = 0;
-        livePlay = true;
     
 };
 
